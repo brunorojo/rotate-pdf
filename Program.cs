20fd2caccfa6fbd52a48a3f9aebe8f6ac43a98d9
Bruno Rojo <brunoasrojo@gmail.com>
2024-06-15 11:30:29 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using PdfSharpCore.Pdf;
+using PdfSharpCore.Pdf.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int MaxFileLength = 128;
+
+        // Verifica quantidade de argumentos
+        if (args.Length != 1)
+        {
+            Console.WriteLine("\n[ERRO] Comando inválido! Utilize: dotnet run [meu_arquivo.pdf]");
+            return;
+        }
+
+        //Verifica tamanho do arquivo
+        if (args[0].Length > MaxFileLength)
+        {
+            Console.WriteLine($"\n[Erro] O arquivo '{args}' excede o limite máximo de {MaxFileLength} caracteres.");
+            return;
+        }
+
+        string inputFilePath = args[0];
+        string outputFilePath = "OUTPUT_" + inputFilePath;
+
+        try
+        {
+            // Leia o arquivo PDF
+            using (PdfDocument document = PdfReader.Open(inputFilePath, PdfDocumentOpenMode.Modify))
+            {
+                // Girar cada página do documento PDF em 180º
+                foreach (PdfPage page in document.Pages)
+                {
+                    page.Rotate = (page.Rotate + 180) % 360;
+                }
+
+                // Salve o documento alterado
+                document.Save(outputFilePath);
+            }
+
+
+            Console.WriteLine($"\nArquivo PDF girado com sucesso! {outputFilePath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"\n[ERRO] Erro ao processar o arquivo: {ex.Message}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Rotate PDF 180º
+
+> Projeto console em _.NET8_ com _PdfSharpCore_ para girar arquivos PDF em 180º.
+
+## Ao executar o comando, um novo arquivo é criado com cada página do PDF girado em 180º:
+
+```shell
+dotnet run meu_arquivo.pdf
+```
