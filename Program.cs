2605d7eeac680560405d8b3a3355d4752effaa9c
B.Rojo <brunoasrojo@gmail.com>
2025-05-13 06:09:13 -0300

    Refatoracao

--- Program.cs
+++ Program.cs
@@ -1,58 +1,55 @@
-﻿using PdfSharpCore.Pdf;
-using PdfSharpCore.Pdf.IO;
-using RotatePdf;
+﻿using PdfSharpCore.Pdf.IO;
+using RotatePdf.Validations;
 
-class Program
+namespace RotatePdf;
+
+internal static class Program
 {
-    static void Main(string[] args)
-    {
+    private static void Main(string[] args)
+    { 
         //Verifica argumentos se null ou vazio
+        Validate.IsArgsNullOrEmpty(args);
+        Validate.IsFileExist(args[0]);
 
-        var validate = new Validate();
-        validate.IsArgsNullOrEmpty(args);
-        validate.IsFileExist(args[0]);
-
-        string input = args[0].ToString();
+        var input = args[0];
         Console.Clear();
 
-        //args[0] = "C:\\Users\\bruno\\source\\RotatePdf\\arquivo-pdf.pdf";//simulando arrasto de arquivo
+        // [DEBUG] Testes de simulacao do arrasto de arquivo para o executavel
+        //args[0] = "C:\\Users\\bruno\\source\\RotatePdf\\arquivo-pdf.pdf"; 
 
+        var inputFileName = Path.GetFileName(input);
+        var inputFolderName = Path.GetDirectoryName(input);
+        var outputFileName = "OUTPUT_" + inputFileName;
+        var outputFilePath = Path.Combine(inputFolderName!, outputFileName);
 
-
-        string inputFileName = Path.GetFileName(input);
-        string inputFolderName = Path.GetDirectoryName(input);
-        string outputFileName = "OUTPUT_" + inputFileName;
-        string outputFilePath = Path.Combine(inputFolderName, outputFileName);
-
-        Console.WriteLine($"[INFO] ..........input: [{input}]");
-        Console.WriteLine($"[INFO] ..inputFileName: [{inputFileName}]");
-        Console.WriteLine($"[INFO] inputFolderName: [{inputFolderName}]");
-        Console.WriteLine($"[INFO] .outputFileName: [{outputFileName}]");
-        Console.WriteLine($"[INFO] .outputFilePath: [{outputFilePath}]");
-
+        Console.WriteLine($"[DEBUG] ..........input: [{input}]");
+        Console.WriteLine($"[DEBUG] ..inputFileName: [{inputFileName}]");
+        Console.WriteLine($"[DEBUG] inputFolderName: [{inputFolderName}]");
+        Console.WriteLine($"[DEBUG] .outputFileName: [{outputFileName}]");
+        Console.WriteLine($"[DEBUG] .outputFilePath: [{outputFilePath}]");
 
         try
         {
-            // Leia o arquivo PDF
-            using (PdfDocument document = PdfReader.Open(inputFileName, PdfDocumentOpenMode.Modify))
+            // Lê arquivo PDF
+            using (var document = PdfReader.Open(inputFileName, PdfDocumentOpenMode.Modify))
             {
-                // Girar cada página do documento PDF em 180º
-                foreach (PdfPage page in document.Pages)
+                // Gira cada página do documento PDF em 180º
+                foreach (var page in document.Pages)
                 {
                     page.Rotate = (page.Rotate + 180) % 360;
                 }
 
-                // Salve o documento alterado
+                // Salvar
                 document.Save(outputFileName);
             }
 
             Console.WriteLine($"\nArquivo PDF girado com sucesso! {outputFileName}");
-
         }
         catch (Exception ex)
         {
             Console.WriteLine($"\n[ERRO] Erro ao processar arquivo: {ex.Message}");
         }
+
         Console.ReadKey();
     }
 }
---
+++ Validations/Validate.cs
@@ -0,0 +1,32 @@
+﻿namespace RotatePdf.Validations;
+
+internal static class Validate
+{
+    internal static void IsArgsNullOrEmpty(string[] args)
+    {
+        // Verifica se o argumento é NULL ou VAZIO
+        if (args.Length == 0)
+        {
+            LogError("Nenhum argumento fornecido! Comando: dotnet run [meu_arquivo.pdf]");
+        }
+    }
+
+    internal static void IsFileExist(string args)
+    {
+        // Verifica se o arquivo EXISTE
+        if (!File.Exists(args))
+        {
+            LogError($"Arquivo {args} não encontrado.");
+        }
+    }
+
+
+    private static void LogError(string msg)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"\n[ERRO] {msg} \n");
+        Console.ReadKey();
+        Console.ResetColor();
+        Environment.Exit(0);
+    }
+}
